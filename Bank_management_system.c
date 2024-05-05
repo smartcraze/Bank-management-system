2adbea109f883bfa83cdd80ba66d92f2fb09cea3
smartcraze <smartcraze>
2024-05-05 23:08:01 +0530

    Added structure for Bank Management System and prompt for user input
    The diff includes the addition of a struct named "Bank\_management\_system" with fields for account number, name, balance, deposit, withdraw, and total balance. Also,

--- Bank_management_system.c
+++ Bank_management_system.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main() {
-  
+int main()
+{
+    struct Bank_management_system
+    {
+        int account_number;
+        char name[20];
+        int balance;
+        int deposit;
+        int withdraw;
+        int total_balance;
+    };
+
+    int n;
+    printf("Enter the ")
+    scanf("%d", &n);
+    
+
+
     return 0;
 }
