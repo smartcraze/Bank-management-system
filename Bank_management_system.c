af6cdbd6f194f6504aa53d4d945ff2b05ae29f30
Zaheed <szasaa10@gmail.com>
2024-05-05 23:19:32 +0530

    "Added a switch statement to handle user choices in the bank management system"
    This commit adds a new variable `choice` to store the user's selected option from the main menu. A switch statement is then added to handle the different options

--- Bank_management_system.c
+++ Bank_management_system.c
@@ -13,7 +13,7 @@ struct Bank_management_system
 int main()
 {
     struct Bank_management_system sbi;
-
+    int choice;
     while (1)
     {
         printf("x=====================Welcome to Sbi bank account===================x\n");
@@ -22,9 +22,14 @@ int main()
         printf("3. Withdraw\n");
         printf("4. Check Balance\n");
         printf("5. Exit\n");
-
-
+        printf("Enter the Number which you require");
+        scanf("%d",&choice);
+        switch (choice)
+        {
+        case 1:
+        
     }
+    
 
     return 0;
 }
